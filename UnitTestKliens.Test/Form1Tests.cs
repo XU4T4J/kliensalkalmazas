b3c144f9fc385df30fd8153266ee51815276082e
XU4T4J <daniel.bacsi@stud.uni-corvinus.hu>
2024-04-24 01:56:20 +0200

    alap felépítve

---
+++ UnitTestKliens.Test/Form1Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RF_Kliensalkalmazás;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NUnit.Framework;
+
+
+namespace RF_Kliensalkalmazás.Tests
+{
+    //public static class ApiCallHelper
+    //{
+    //    public static Api apicall()
+    //    {
+    //        string apiUrl = "http://20.234.113.211:8095/";
+    //        string apikey = "1-99c18ed4-5034-453f-8b77-f077f81d6b89";
+
+    //        Api proxy = new Api(apiUrl, apikey);
+    //        return proxy;
+    //    }
+    //}
+    public class Form1Tests
+    {
+        [Test]
+        public void TestApicall()
+        {
+            ////Arrange
+            //var expectedApiUrl = "http://20.234.113.211:8095/";
+            //var expectedApiKey = "1-99c18ed4-5034-453f-8b77-f077f81d6b89";
+
+            ////Act
+            //var api = ApiCallHelper.apicall();
+            ////Assert
+            //Assert.AreEqual(expectedApiUrl, api.ApiUrl);
+
+        }
+    }
+}
---
+++ UnitTestKliens.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestKliens.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestKliens.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c16b6d01-9cac-4242-be2b-85b2314d0c9c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
