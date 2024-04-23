b1e49826c77f0a1750d000c050280e6389ae160a
Máté Szabó <mate.szabo6@stud.uni-corvinus.hu>
2024-04-23 18:00:19 +0200

    v.0.1

---
+++ RF_Kliensalkalmazás/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace RF_Kliensalkalmazás
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBoxProducts = new System.Windows.Forms.CheckedListBox();
+            this.buttonDOWN = new System.Windows.Forms.Button();
+            this.buttonUP = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // checkedListBoxProducts
+            // 
+            this.checkedListBoxProducts.FormattingEnabled = true;
+            this.checkedListBoxProducts.Location = new System.Drawing.Point(39, 32);
+            this.checkedListBoxProducts.Name = "checkedListBoxProducts";
+            this.checkedListBoxProducts.Size = new System.Drawing.Size(328, 349);
+            this.checkedListBoxProducts.TabIndex = 0;
+            this.checkedListBoxProducts.SelectedIndexChanged += new System.EventHandler(this.checkedListBoxProducts_SelectedIndexChanged);
+            // 
+            // buttonDOWN
+            // 
+            this.buttonDOWN.Location = new System.Drawing.Point(639, 358);
+            this.buttonDOWN.Name = "buttonDOWN";
+            this.buttonDOWN.Size = new System.Drawing.Size(134, 23);
+            this.buttonDOWN.TabIndex = 1;
+            this.buttonDOWN.Text = "Leárazás";
+            this.buttonDOWN.UseVisualStyleBackColor = true;
+            this.buttonDOWN.Click += new System.EventHandler(this.buttonDOWN_Click);
+            // 
+            // buttonUP
+            // 
+            this.buttonUP.Location = new System.Drawing.Point(447, 358);
+            this.buttonUP.Name = "buttonUP";
+            this.buttonUP.Size = new System.Drawing.Size(134, 23);
+            this.buttonUP.TabIndex = 2;
+            this.buttonUP.Text = "Ár növelése";
+            this.buttonUP.UseVisualStyleBackColor = true;
+            this.buttonUP.Click += new System.EventHandler(this.buttonUP_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(639, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(511, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Árváltoztatás mértéke:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(385, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 290);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonUP);
+            this.Controls.Add(this.buttonDOWN);
+            this.Controls.Add(this.checkedListBoxProducts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBoxProducts;
+        private System.Windows.Forms.Button buttonDOWN;
+        private System.Windows.Forms.Button buttonUP;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ RF_Kliensalkalmazás/Form1.cs
@@ -0,0 +1,102 @@
+﻿
+using Hotcakes.Commerce.Marketing.PromotionQualifications;
+using Hotcakes.CommerceDTO.v1;
+using Hotcakes.CommerceDTO.v1.Catalog;
+using Hotcakes.CommerceDTO.v1.Client;
+using Hotcakes.CommerceDTO.v1.Contacts;
+using Hotcakes.CommerceDTO.v1.Membership;
+using Hotcakes.CommerceDTO.v1.Orders;
+using Hotcakes.Web;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RF_Kliensalkalmazás
+{
+    public partial class Form1 : Form
+    {
+        private Api api;
+        public static Api apicall()
+        {
+            string apiUrl = "http://20.234.113.211:8095/";
+            string apikey = "1-99c18ed4-5034-453f-8b77-f077f81d6b89";
+
+            Api proxy = new Api(apiUrl, apikey);
+            return proxy;
+        }
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            api = apicall();
+
+            ApiResponse<List<ProductDTO>> DTO = api.ProductsFindAll();
+            Dictionary<string, string> products = new Dictionary<string, string>();
+            for (int i = 0; i < 100; i++)
+            {
+                string id = DTO.Content[i].Bvin.ToString();
+                string name = DTO.Content[i].ProductName.ToString();
+
+                products.Add(id, name);
+            }
+
+            foreach (string ne in products.Values)
+            {
+                checkedListBoxProducts.Items.Add(ne);
+            }
+
+            //int x = Convert.ToInt32(textBox1.Text);
+
+            List<string> selectedNames = new List<string>();
+
+            foreach (string sname in checkedListBoxProducts.SelectedItems) 
+            { 
+                selectedNames.Add(sname);
+            }
+            foreach (string nasde in selectedNames)
+            {
+                listBox1.Items.Add(nasde);
+            }
+
+        }
+
+        private void checkedListBoxProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listatoltes();
+        }
+        
+        private void buttonUP_Click(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+
+
+        private void buttonDOWN_Click(object sender, EventArgs e)
+        {
+            int down = Convert.ToInt32(textBox1.Text);
+        }
+
+
+        public void listatoltes()
+        {
+            
+        }
+    }
+        
+}
